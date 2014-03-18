555fabed365e5c91b1fa12d42fa74ca7dd4a9ba9
Jack <jmcsween@marine.rutgers.edu>
2014-03-18 14:35:14 -0400

    Basic outline of modules and functions.  Contains my first nosetest

---
+++ pythonDemoScript.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python 
+
+'''This module was created to demonstrate testing at the 3/14 NYU 
+Software Bootcamp'''
+
+def coordinates2region():
+	'''Takes an input '''
+	pass
+
+def main (): 
+	'''This is the main controller for my module'''
+	print 'Hi!'
+
+if __name__ == '__main__':
+	main ()
+
+
+
+
+
+
---
+++ testCoordinates.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python 
+
+'''This is the nosetest module for the pythonDemoScript.py''' 
+
+def test_foo():
+	'''Bar'''
+	pass 
+	
